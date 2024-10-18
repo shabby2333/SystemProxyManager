aca4af084b035a394a6cb21b6c452eb5f4873043
13089 <13089@SHABBY-PC>
2024-10-18 21:19:32 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SystemProxyManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnStatus = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbProxyAddress = new System.Windows.Forms.ToolStripTextBox();
+            this.btnExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnAutoStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnStatus,
+            this.tbProxyAddress,
+            this.btnAutoStart,
+            this.btnExit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(221, 117);
+            // 
+            // btnStatus
+            // 
+            this.btnStatus.Name = "btnStatus";
+            this.btnStatus.Size = new System.Drawing.Size(220, 22);
+            this.btnStatus.Text = "系统代理";
+            this.btnStatus.Click += new System.EventHandler(this.btnStatus_Click);
+            // 
+            // tbProxyAddress
+            // 
+            this.tbProxyAddress.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.tbProxyAddress.Name = "tbProxyAddress";
+            this.tbProxyAddress.Size = new System.Drawing.Size(160, 23);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(220, 22);
+            this.btnExit.Text = "退出";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnAutoStart
+            // 
+            this.btnAutoStart.Name = "btnAutoStart";
+            this.btnAutoStart.Size = new System.Drawing.Size(220, 22);
+            this.btnAutoStart.Text = "开机启动";
+            this.btnAutoStart.Click += new System.EventHandler(this.btnAutoStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 37);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem btnStatus;
+        private System.Windows.Forms.ToolStripMenuItem btnExit;
+        private System.Windows.Forms.ToolStripTextBox tbProxyAddress;
+        private System.Windows.Forms.ToolStripMenuItem btnAutoStart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using IWshRuntimeLibrary;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemProxyManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            InitializeComponent();
+        }
+
+        [DllImport("wininet.dll")]
+        private static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int dwBufferLength);
+        private const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+        private const int INTERNET_OPTION_REFRESH = 37;
+        private bool settingsReturn, refreshReturn;
+        RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+        private AutoStart autoStart = new AutoStart()
+        {
+            QuickName = "ProxyManager",
+            QuickDescribe = "ProxyManager",
+            WindowStyle = WshWindowStyle.WshMinimizedFocus
+        };
+
+        public void CheckProxyOpened()
+        {
+            btnStatus.Checked = Convert.ToInt32(registry.GetValue("ProxyEnable")) == 1;
+            tbProxyAddress.Text = Convert.ToString(registry.GetValue("ProxyServer"));
+            btnAutoStart.Checked = autoStart.GetAutoStart();
+        }
+
+        private void btnStatus_Click(object sender, EventArgs e)
+        {
+            btnStatus.Checked = !btnStatus.Checked;
+            ChangeProxyOpen(btnStatus.Checked);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckProxyOpened();
+        }
+
+        private void btnAutoStart_Click(object sender, EventArgs e)
+        {
+            btnAutoStart.Checked = !btnAutoStart.Checked;
+            autoStart.SetAutoStart(btnAutoStart.Checked);
+        }
+
+        public void ChangeProxyOpen(bool open)
+        {
+            registry.SetValue("ProxyEnable", open? 1: 0);
+            if(open && !String.IsNullOrEmpty(tbProxyAddress.Text))
+                registry.SetValue("ProxyServer", tbProxyAddress.Text);
+
+            // These lines implement the Interface in the beginning of program 
+            // They cause the OS to refresh the settings, causing IP to realy update
+            settingsReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            refreshReturn = InternetSetOption(IntPtr.Zero, INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+
+        }
+    }
+}
